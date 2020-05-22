d1de27198c208a2098a4d3bb45e496794ed11ca7
vlad <vladov3000@gmail.com>
2020-05-22 16:02:19 -0700

    updated reana yml

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 rm -r workdir
 WORKFLOW=${1:-workflows/workflow.yml}
-yadage-run workdir $WORKFLOW inputs/input.yml -d initdir=$PWD/inputs &> /dev/null
+yadage-run workdir $WORKFLOW inputs/input.yml -d initdir=$PWD/inputs
 
